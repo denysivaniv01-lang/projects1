9047300a37b74cdbdecc2bef418e11b1f4374d60
denysivaniv01-lang <denysivaniv01@gmail.com>
2026-02-13 22:33:50 +0200

    update

---
+++ caltulator.py
@@ -0,0 +1,44 @@
+a = 20
+b = 6
+
+result1 = a + b  # 20 + 6 = 26
+result2 = a - b  # 20 - 6 = 14
+result3 = a * b  # 20 * 6 = 120
+result4 = a / b  # 20 / 6 = 3,3333
+result5 = a // b  # 20 // 6 = 3
+result6 = a % b  # 20 % 6 = 2
+result7 = a ** b  # 20 ** 6 = 64000000
+
+print(result1)
+print("-" * 40)
+print(result2)
+print("-" * 40)
+print(result3)
+print("-" * 40)
+print(result4)
+print("-" * 40)
+print(result5)
+print("-" * 40)
+print(result6)
+print("-" * 40)
+print(result7)
+print("-" * 40)
+print("\n")
+
+score = 0
+print(score)
+score += 10
+print(score)
+score += 5
+print(score)
+score -= 3
+print(score)
+score *= 2
+print(score)
+print("\n")
+
+total = 99.99 * 1.20  # множення
+round(total, 2)  # множення 99.99 на 1.20 = 119.987
+print(min(10, 2, 6, 4, 9, 7, 1))  # знаходження найменшого значеня 1
+print(max(10, 2, 6, 4, 9, 7, 1))  # знаходження найбіль 10
+print(round(5.5))  # вийде 6 бо ми закрюглоємо
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ a = True
 c = int(a)
 print(c)
 
-#a = 10
-#b = "book"
-#c = a * b
-#print(c)
+# a = 10
+# b = "book"
+# c = a * b
+# print(c)
